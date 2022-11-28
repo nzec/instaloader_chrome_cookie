cfba57d160ca36e4a97837cbdeaeb16941fbe4d2
Aditya Dutt <duttaditya18@gmail.com>
2022-11-28 17:55:45 +0530

    Fixed problem when using GNOME or KDE

--- README.txt
+++ README.txt
@@ -3,19 +3,16 @@
 Works only on Linux and Darwin.
 
 If not using Chromium, specify cookie file using the -c option.
+
+If your browser is using GNOME Keyring or KWallet you will need to specify the password using the -p flag.
+See this for more info: https://chromium.googlesource.com/chromium/src/+/master/docs/linux/password_storage.md
+
 For example for Brave Browser do this:
-python instaloader_chrome_cookie.py -c ~/.config/BraveSoftware/Brave-Browser/Default/Cookies
+./instaloader_chrome_cookie.py -c ~/.config/BraveSoftware/Brave-Browser/Default/Cookies -p [password]
 
 Optionally specify the instaloader session file using the -f option.
 
 Usage:
-	git clone https://github.com/nzec/instaloader_chrome_cookie.git
+	git clone https://github.com/nzec/instaloader_chrome_cookie.git && cd instaloader_chrome_cookie
 	pip install -r requirements.txt
-	python instagram_chrome_cookie.py [-h] [-c COOKIEFILE] [-f SESSIONFILE]
-
-Usage (with venv):
-	git clone https://github.com/nzec/instaloader_chrome_cookie.git
-	python -m venv venv
-	venv/bin/pip install -r requirements.txt
-	venv/bin/python instagram_chrome_cookie.py [-h] [-c COOKIEFILE] [-f SESSIONFILE]
-
+	./instagram_chrome_cookie.py [-h] [-c COOKIEFILE] [-f SESSIONFILE] [-p PASSWORD]
--- instaloader_chrome_cookie.py
+++ instaloader_chrome_cookie.py
@@ -1,9 +1,13 @@
+#!/usr/bin/env python3
+
 '''instaloader_chrome_cookie.py
 Works only on Linux and Darwin.
 
 If not using Chromium, specify cookie file using the -c option.
+If your browser is using GNOME Keyring or KWallet you will need to specify the password using the -p flag.
+
 For example for Brave Browser do this:
-python instaloader_chrome_cookie.py -c ~/.config/BraveSoftware/Brave-Browser/Default/Cookies
+./instaloader_chrome_cookie.py -c ~/.config/BraveSoftware/Brave-Browser/Default/Cookies -p [password]
 
 Optionally specify the instaloader session file using the -f option.
 '''
@@ -14,19 +18,30 @@ from instaloader import Instaloader
 
 url = 'https://instagram.com'
 
-def import_session(cookiefile, sessionfile):
+def import_session(cookiefile, sessionfile, password):
     if cookiefile:
-        cookie_data = chrome_cookies(url, cookie_file=cookiefile)
+        if password:
+            try:
+                cookie_data = chrome_cookies(url, cookie_file=cookiefile, password=password)
+            except:
+                raise SystemError("The password you specified is probably wrong.")
+        else:
+            try:
+                cookie_data = chrome_cookies(url, cookie_file=cookiefile)
+            except:
+                raise SystemError("Maybe you need to specify password.")
     else:
-        cookie_data = chrome_cookies(url)
-    # print(cookie_data)
+        if password:
+            cookie_data = chrome_cookies(url, password=password)
+        else:
+            cookie_data = chrome_cookies(url) 
 
     instaloader = Instaloader(max_connection_attempts=1)
     instaloader.context._session.cookies.update(cookie_data)
 
     username = instaloader.test_login()
     if not username:
-        raise SystemExit("Not logged in. Are you logged in successfully?")
+        raise SystemError("Not logged in. Are you logged in successfully?")
 
     print("Imported session cookie for {}.".format(username))
     instaloader.context.username = username
@@ -36,8 +51,9 @@ if __name__ == "__main__":
     p = ArgumentParser()
     p.add_argument("-c", "--cookiefile")
     p.add_argument("-f", "--sessionfile")
+    p.add_argument("-p", "--password")
     args = p.parse_args()
     try:
-        import_session(args.cookiefile, args.sessionfile)
+        import_session(args.cookiefile, args.sessionfile, args.password)
     except Exception as e:
         raise SystemExit("Cookie import failed: {}".format(e))
